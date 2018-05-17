9a30dd30b5826aa0b5e7f8d5d8fbd67e12014dab
Benjamin Huang <bhuang@rms.com>
2018-05-17 15:28:22 -0700

    add types

---
+++ graphql-magic.md
@@ -0,0 +1,7 @@
+## Type
+    This is where we define what we want to respond to the user that is making the request, we can add some validations there and also we can define what type of data we are going to return.
+- There are different type of types, we should import those from graphql and set what kind of data is the one that we are returning.
+- Each type must be a GraphQLObjectType which will have multiple properties
+
+## Query
+    The queries are where we want to set the /GET that we want to let GraphQL do to our database, we set in the query the type of response that we want to give, so we can be sure which data we want to share.
--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const cors = require('cors')
 //  PostgreSQL client for Node.js.
-const pg = require('pg') 
+const pg = require('pg')
 // create a new pool to the database
 const pgPool = new pg.Pool({ database: 'mydb' })
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config({ path: 'development.env' })
+  require('dotenv').config({ path: 'development.env' })
 }
 
 app.set('port', process.env.PORT || 7000)
@@ -19,15 +19,15 @@ const ncSchema = require('./schema')
 // We  use the context to connect to the db and get the req data.
 // Each query or mutation has a type, that is the type of respond to the request.
 app.use('/graphql', cors(), (req, res) => {
-    graphqlHTTP({
-        schema: ncSchema,
-        graphiql: process.env.NODE_ENV === 'development',
-        context: { pgPool, req }
-    })(req, res)
+  graphqlHTTP({
+    schema: ncSchema,
+    graphiql: process.env.NODE_ENV === 'development',
+    context: { pgPool, req }
+  })(req, res)
 })
 
 const server = app.listen(app.get('port'), () => {
-    console.log(`Server running -> PORT ${server.address().port}`)
+  console.log(`Server running -> PORT ${server.address().port}`)
 })
 
 module.exports = app
