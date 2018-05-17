5b58a6086ce373c9769fe5c9cbb4a7c7be704b93
Benjamin Huang <bhuang@rms.com>
2018-05-17 14:42:37 -0700

    add files

---
+++ README.md
@@ -0,0 +1,13 @@
+## Refernce 
+- Create your first GraphQL app with nodejs and set an auth middleware
+    - https://itnext.io/create-your-first-graphql-app-and-set-an-auth-middleware-2dd841f714c2
+
+
+## Setup
+```
+npm init -y
+npm i -s cors dotenv express express-graphql graphql jsonwebtoken pg
+npm i -D nodemon standard
+```
+
+- pg: PostgreSQL client for Node.js. 
---
+++ src/App.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
+//  PostgreSQL client for Node.js.
+const pg = require('pg') 
+// create a new pool to the database
+const pgPool = new pg.Pool({ database: 'mydb' })
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: 'development.env' })
+}
+
+app.set('port', process.env.PORT || 7000)
+
+const ncSchema = require('./schema')
+
+//Set GraphQL as middleware
+app.use('/graphql', cors(), (req, res) => {
+    graphqlHTTP({
+        schema: ncSchema,
+        graphiql: process.env.NODE_ENV === 'development',
+        context: { pgPool, req }
+    })(req, res)
+})
+
+const server = app.listen(app.get('port'), () => {
+    console.log(`Server running -> PORT ${server.address().port}`)
+})
+
+module.exports = app
