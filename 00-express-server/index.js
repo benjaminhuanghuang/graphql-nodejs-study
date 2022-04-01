515c8d5c458f44c7603a39c3c5fb9f0fa229d7d4
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-31 21:29:13 -0700

    refactor

--- 00-express-server/index.js
+++ 00-express-server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-const PORT = 8964
+const PORT = 8964;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () => {
   console.log(`Express sever is running at port ${PORT}`);
